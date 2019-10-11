11302568cb64be79707d008f6a09d7fb45c1079c
MinaRaoufAziz <55676799+MinaRaoufAziz@users.noreply.github.com>
2019-10-11 17:57:57 +0200

    Update stdtypes.h

--- stdtypes.h
+++ stdtypes.h
@@ -1,11 +1,24 @@
 #ifndef stdTypes_h
-#define stdTypes_h
 typedef unsigned char uint8;
 typedef signed char sint8;
 typedef unsigned short int uint16;
 typedef signed short int sint16;
 typedef unsigned long int uint32;
 typedef signed long int sint32;
+typedef unsigned long long int uint64;
+typedef signed long long int sint64;
 typedef float float32;
-typedef double f64;
+typedef double float64;
+typedef long double float128;
+typedef unsigned char* pu8;
+typedef signed char* ps8;
+typedef unsigned short int* pu16;
+typedef signed short int* ps16;
+typedef unsigned long int* pu32;
+typedef signed long int* ps32;
+typedef unsigned long long int* pu64;
+typedef signed long long int* ps64;
+typedef float* pf32;
+typedef double* pf64;
+typedef long double* pf128;
 #endif
